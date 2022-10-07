e3779fa82ca761c08a2730c1610efa8b70344891
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2022-10-07 12:12:45 +0200

    feat: chain config in spreadsheet

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { GoogleSheet } from "gsheet-object";
 import axios from "axios";
 import * as web3Utils from "web3-utils";
 
-interface IWalletAlertConfig {
+type IWalletAlertConfig = {
   name: string;
   address: string;
   threshold: string;
@@ -10,31 +10,24 @@ interface IWalletAlertConfig {
   slackHook: string;
   balance: string;
   delta: string;
-}
-
-const urls: Record<string, string> = {
-  rinkeby: "https://api-rinkeby.etherscan.io",
-  mainnet: "https://api.etherscan.io",
-  xdai: "https://blockscout.com/xdai/mainnet",
-};
-const explorerUrls: Record<string, string> = {
-  rinkeby: "https://rinkeby.etherscan.io",
-  mainnet: "https://etherscan.io",
-  xdai: "https://blockscout.com/xdai/mainnet",
 };
 
-const currencies: Record<string, string> = {
-  rinkeby: "ETH-rinkeby",
-  mainnet: "ETH",
-  xdai: "xDAI",
+type IChainConfig = {
+  name: string;
+  explorerUrl: string;
+  apiUrl: string;
+  currency: string;
 };
+
+type IChainMap = Record<string, IChainConfig>;
 
 const sendAlert = async (
   text: any,
-  { name, address, balance, network, slackHook }: IWalletAlertConfig
+  { name, address, balance, network, slackHook }: IWalletAlertConfig,
+  chains: IChainMap
 ) => {
-  const baseUrl = explorerUrls[network.toLowerCase()];
-  const currency = currencies[network.toLowerCase()];
+  const baseUrl = chains[network.toLowerCase()].explorerUrl;
+  const currency = chains[network.toLowerCase()].currency;
 
   await axios.post(slackHook, {
     attachments: [
@@ -63,15 +56,8 @@ const sendAlert = async (
   });
 };
 
-const getClient = (network: string) => {
-  const baseURL = urls[network.toLowerCase()] || urls.mainnet;
-  const client = axios.create({
-    baseURL,
-    params: baseURL.includes("etherscan")
-      ? { apiKey: process.env.ETHERSCAN_API_KEY }
-      : {},
-  });
-  return client;
+const getClient = (baseURL: string, apiKey?: string) => {
+  return axios.create({ baseURL, params: { apiKey } });
 };
 
 export const getAlertLevel = (
@@ -101,7 +87,7 @@ export const getAlertLevel = (
   return "error";
 };
 
-const processWallet = async (wallet: IWalletAlertConfig) => {
+const processWallet = async (wallet: IWalletAlertConfig, chains: IChainMap) => {
   const {
     name,
     address,
@@ -110,7 +96,14 @@ const processWallet = async (wallet: IWalletAlertConfig) => {
     balance: currentBalance,
     delta: deltaStr,
   } = wallet;
-  const api = getClient(network);
+  if (!chains[network]) {
+    console.warn("unknown network!", network);
+    return;
+  }
+  const api = getClient(
+    chains[network].apiUrl,
+    process.env[`ETHERSCAN_API_KEY_${network.toUpperCase()}`]
+  );
   const response = await api.get("/api", {
     params: {
       module: "account",
@@ -143,7 +136,7 @@ const processWallet = async (wallet: IWalletAlertConfig) => {
     console.warn(
       `low balance on wallet ${name} (${address}): ${result.balance}.`
     );
-    sendAlert(`:alert: Low balance on wallet ${name}`, result);
+    sendAlert(`:alert: Low balance on wallet ${name}`, result, chains);
   } else {
     console.log(`balance on wallet ${name} (${address}): ${newBalance}`);
   }
@@ -153,9 +146,15 @@ const processWallet = async (wallet: IWalletAlertConfig) => {
 export const main = async () => {
   const sheet = await GoogleSheet.load<IWalletAlertConfig>("WalletAlerts");
   const wallets = await sheet.getData();
+  const chainsSheet = await GoogleSheet.load<IChainConfig>("Chains");
+  const chainData = await chainsSheet.getData();
+  const chains = chainData.reduce(
+    (acc, curr) => ({ ...acc, [curr.name]: curr }),
+    {} as IChainMap
+  );
 
   for (const wallet of wallets) {
-    const result = await processWallet(wallet);
+    const result = await processWallet(wallet, chains);
     if (result) {
       console.log(`updating ${result.name} to ${result.balance}`);
       await sheet.update(wallet._row, "balance", result.balance);
